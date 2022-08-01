d1c9400f1503c6b37d12c6c7d5559924d5c2541c
Siddhant <siddhantp457@gmail.com>
2022-08-01 12:01:37 +0530

    Added: Jumbotron Cards + normalize.css

--- js/index.js
+++ js/index.js
@@ -8,8 +8,3 @@ toggleBtn.addEventListener("click", () => {
     navbarItems.classList.toggle("active");
 });
 
-// Topup disappearing code
-const topupBtn = document.querySelector("#topupcloser");
-topupBtn.addEventListener("click", () => {
-    document.querySelector(".topup").style.display = "none";
-});
